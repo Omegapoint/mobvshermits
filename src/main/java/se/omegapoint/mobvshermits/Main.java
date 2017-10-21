45866fe79b0b5fe8c38f7b517179710043be0dff
Martin Carlsson <martin.carlsson@omegapoint.se>
2017-10-21 21:19:37 +0200

    Removed jersey dependencies.

--- src/main/java/se/omegapoint/mobvshermits/Main.java
+++ src/main/java/se/omegapoint/mobvshermits/Main.java
@@ -6,22 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.Response;
-
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
-    }
-
-    private static String getLocationsByName(Config config, String name) {
-        final Response response = ClientBuilder.newClient()
-                .target("https://api.resrobot.se/v2/location.name?key=" + config.resrobotKey() + "&input=" +
-                        name + "&format=json")
-                .request().get();
-        return response.readEntity(String.class);
     }
 
     @Bean
--- src/main/java/se/omegapoint/mobvshermits/rest/StopResource.java
+++ src/main/java/se/omegapoint/mobvshermits/rest/StopResource.java
@@ -1,5 +1,6 @@
 package se.omegapoint.mobvshermits.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import se.omegapoint.mobvshermits.Config;
 import se.omegapoint.mobvshermits.json.StopLocation;
 import se.omegapoint.mobvshermits.json.StopLocationsResponse;
 
-import javax.inject.Inject;
+import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import java.net.URI;
 import java.util.Collection;
@@ -20,13 +21,14 @@ public class StopResource {
     private final RestTemplate restTemplate;
     private final Config config;
 
-    @Inject
+    @Autowired
     StopResource(RestTemplate restTemplate, Config config) {
         this.restTemplate = restTemplate;
         this.config = config;
     }
 
-    @RequestMapping("/stop")
+    @RequestMapping
+    @Path("/stop")
     public Collection<StopLocation> getStopsByName(@QueryParam("query") String query) {
         final URI uri = UriComponentsBuilder.fromUriString(HTTPS_API_RESROBOT_SE_V2 + "/location.name")
                 .queryParam("key", config.resrobotKey())
